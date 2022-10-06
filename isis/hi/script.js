fd0728cefb0f856a77d09f89b4893a876388d1d8
lrolofson <lrolofson@gmail.com>
2022-10-05 21:44:16 -0700

    Create script.js

---
+++ isis/hi/script.js
@@ -0,0 +1,18 @@
+myMsg = "hi";
+var now = new Date();
+var night = true; // this doesn't do anything yet. probably ever. i know, i know.
+const pText = document.getElementById("p1");
+
+//please don't bully me about how many if statements i'm using here
+//feedback is obviously appreciated just be nice please ok thanks
+if (now.getHours() < 2) myMsg = "Up late!";
+else if (now.getHours() < 3) myMsg = "THE WITCHING HOUR IS AT HAND";
+else if (now.getHours() < 5) myMsg = "Hey there, early bird!";
+else if (now.getHours() <= 11) myMsg = "Mornin'!";
+else if (now.getHours() < 13) myMsg = "Lunchtime!";
+else if (now.getHours() < 17) myMsg = "Good afternoon!";
+else if (now.getHours() < 21) myMsg = "Good evening!";
+else if (now.getHours() < 24) myMsg = "Bedtime!";
+else myMsg = "i have no idea what time it is lmao";
+
+pText.innerHTML = myMsg;
