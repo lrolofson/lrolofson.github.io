77273c80d96004db9723ec87886f0b6c48af422c
lrolofson <lrolofson@gmail.com>
2022-10-05 22:39:19 -0700

    Update script.js
    change wording

--- isis/hi/script.js
+++ isis/hi/script.js
@@ -5,14 +5,14 @@ const pText = document.getElementById("p1");
 
 //please don't bully me about how many if statements i'm using here
 //feedback is obviously appreciated just be nice please ok thanks
-if (now.getHours() < 2) myMsg = "Up late!";
-else if (now.getHours() < 3) myMsg = "THE WITCHING HOUR IS AT HAND";
+if (now.getHours() < 2) myMsg = "Go to bed!";
+else if (now.getHours() < 3) myMsg = "THE WITCHING HOUR IS AT HAND.";
 else if (now.getHours() < 5) myMsg = "Hey there, early bird!";
-else if (now.getHours() <= 11) myMsg = "Mornin'!";
+else if (now.getHours() <= 11) myMsg = "Good mornin'!";
 else if (now.getHours() < 13) myMsg = "Lunchtime!";
 else if (now.getHours() < 17) myMsg = "Good afternoon!";
 else if (now.getHours() < 21) myMsg = "Good evening!";
-else if (now.getHours() < 24) myMsg = "Bedtime!";
+else if (now.getHours() < 24) myMsg = "Good night!";
 else myMsg = "i have no idea what time it is lmao";
 
 pText.innerHTML = myMsg;
